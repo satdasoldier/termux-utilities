485931d2f7895a145066a59878d4c482c49518ad
nanda4 <github.nanda4@snweb.xyz>
2026-04-06 18:02:05 +0100

    initial get from tfl

--- transport/cycle_hire.bash
+++ transport/cycle_hire.bash
@@ -1,4 +1,13 @@
-#!/usr/bin/env bash
+#!/data/data/com.termux/files/usr/bin/bash
 
 
-echo "Hello world!"
+TFL_API_BASE="https://api.tfl.gov.uk"
+TFL_API_URL_TEMPLATE="$TFL_API_BASE/BikePoint"
+
+BIKE_POINTS=("BikePoints_215" "BikePoints_140" "BikePoints_96")
+
+FINAL_URL="$TFL_API_URL_TEMPLATE/BikePoints_215"
+
+
+curl -s -XGET $FINAL_URL
+
